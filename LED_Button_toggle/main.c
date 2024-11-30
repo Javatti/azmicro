27f21058a72edc6e4fe311025abdb8a8d63a8470
mohammad javad <140122356+Javatti@users.noreply.github.com>
2024-11-30 22:06:08 +0330

    add Led Button Project

---
+++ LED_Button_toggle/main.c
@@ -0,0 +1,28 @@
+
+#include <mega32.h>
+#include <delay.h>
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=Out Bit0=In 
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (1<<DDA1) | (0<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=0 Bit0=P 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (1<<PORTA0);
+
+
+while (1)
+      {
+       if (PINA.0 == 0){ 
+       
+       PORTA.1 = ~PORTA.1;
+       delay_ms(100);
+       }
+
+      }
+}
